2bfa703f7b9cf0702323a6a30277491bd1b68bb3
Pierre Laporte <pierre@pingtimeout.fr>
2013-05-28 23:04:53 +0200

    Added Java Agent project

---
+++ profiling-agent/src/main/java/fr/pingtimeout/lockprofiling/LockProfilingAgent.java
@@ -0,0 +1,49 @@
+package fr.pingtimeout.lockprofiling;
+
+import java.lang.instrument.Instrumentation;
+
+public class LockProfilingAgent {
+    private static Instrumentation instrumentation;
+
+    /**
+     * JVM hook to statically load the javaagent at startup.
+     *
+     * After the Java Virtual Machine (JVM) has initialized, the premain method
+     * will be called. Then the real application main method will be called.
+     *
+     * @param args
+     * @param inst
+     * @throws Exception
+     */
+    public static void premain(String args, Instrumentation inst) throws Exception {
+        System.out.printf("LockProfilingAgent : premain method invoked with args: %s and inst: %s", args, inst);
+        instrumentation = inst;
+//        instrumentation.addTransformer(new MyClassFileTransformer());
+    }
+
+    /**
+     * JVM hook to dynamically load javaagent at runtime.
+     *
+     * The agent class may have an agentmain method for use when the agent is
+     * started after VM startup.
+     *
+     * @param args
+     * @param inst
+     * @throws Exception
+     */
+    public static void agentmain(String args, Instrumentation inst) throws Exception {
+        System.out.printf("LockProfilingAgent : agentmain method invoked with args: %s and inst: %s", args, inst);
+        instrumentation = inst;
+//        instrumentation.addTransformer(new MyClassFileTransformer());
+    }
+
+    /**
+     * Programmatic hook to dynamically load javaagent at runtime.
+     */
+    public static void initialize() {
+        System.out.printf("LockProfilingAgent : initialize method invoked");
+        if (instrumentation == null) {
+//            MyJavaAgentLoader.loadAgent();
+        }
+    }
+}
