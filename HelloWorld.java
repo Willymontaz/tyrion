fd5bea8b3f8ae06d8d7976a3c802b307eda2683f
Pierre Laporte <pierre@pingtimeout.fr>
2013-05-29 17:35:28 +0200

    Added some more dummy content with synchronized calls

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class HelloWorld {
     private Object lock = new Object();
 
@@ -6,15 +8,29 @@ public class HelloWorld {
     }
 
     public void synchronizedBlock() {
-        synchronized(lock) {
+        synchronized (lock) {
             System.out.println("World");
         }
     }
 
+    public void externalSynchronizedMethods() {
+        Vector<String> stringVector = new Vector<String>();
+        StringBuffer stringBuffer = new StringBuffer();
+        stringVector.add("Foo");
+        stringVector.add("Foo");
+        for (String s : stringVector) {
+            stringBuffer.append(s);
+        }
+        System.out.println(stringBuffer);
+    }
+
     public static void main(String... args) {
         HelloWorld hello = new HelloWorld();
-        
+
         hello.synchronizedMethod();
         hello.synchronizedBlock();
+        hello.externalSynchronizedMethods();
     }
+
+
 }
