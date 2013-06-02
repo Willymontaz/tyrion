1b16321d15489929985aa2d3f1d57c6e71344ba0
Pierre Laporte <pierre@pingtimeout.fr>
2013-06-02 22:57:40 +0200

    Cleaned code

--- profiling-agent/src/main/java/fr/pingtimeout/lockprofiling/SynchronizedMethodWrapper.java
+++ profiling-agent/src/main/java/fr/pingtimeout/lockprofiling/SynchronizedMethodWrapper.java
@@ -24,21 +24,11 @@ import org.objectweb.asm.commons.AdviceAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SynchronizedMethodWrapper extends AdviceAdapter {
+class SynchronizedMethodWrapper extends AdviceAdapter {
     static Logger LOG = LoggerFactory.getLogger(SynchronizedMethodWrapper.class);
 
     private final String methodName;
 
-    /**
-     * Creates a new {@link org.objectweb.asm.commons.AdviceAdapter}.
-     *
-     * @param api    the ASM API version implemented by this visitor. Must be one
-     *               of {@link org.objectweb.asm.Opcodes#ASM4}.
-     * @param mv     the method visitor to which this adapter delegates calls.
-     * @param access the method's access flags (see {@link org.objectweb.asm.Opcodes}).
-     * @param name   the method's name.
-     * @param desc   the method's descriptor (see {@link org.objectweb.asm.Type Type}).
-     */
     protected SynchronizedMethodWrapper(int api, MethodVisitor mv, int access, String name, String desc) {
         super(Opcodes.ASM4, mv, access, name, desc);
         LOG.debug("Instantiating SynchronizedMethodWrapper for method {} {}", name, desc);
@@ -51,7 +41,7 @@ public class SynchronizedMethodWrapper extends AdviceAdapter {
 
         mv.visitMethodInsn(INVOKESTATIC, "fr/pingtimeout/lockprofiling/LockInterceptor", "enteredSynchronizedMethod", "()V");
 
-        super.onMethodEnter();    //To change body of overridden methods use File | Settings | File Templates.
+        super.onMethodEnter();
     }
 
     @Override
@@ -60,6 +50,6 @@ public class SynchronizedMethodWrapper extends AdviceAdapter {
 
         mv.visitMethodInsn(INVOKESTATIC, "fr/pingtimeout/lockprofiling/LockInterceptor", "leavingSynchronizedMethod", "()V");
 
-        super.onMethodExit(opcode);    //To change body of overridden methods use File | Settings | File Templates.
+        super.onMethodExit(opcode);
     }
 }
