679065b36f5f8968078d75f59b29a3cc7a4d8249
Pierre Laporte <pierre@pingtimeout.fr>
2014-02-23 20:06:59 +0100

    WIP - attempt to add integration tests
    So far, maven-failsafe-plugin does not seem to launch integration tests
    in the integration-test folder. Maybe they are not compiled yet ?

---
+++ agent/src/integration-test/java/fr/pingtimeout/tyrion/agent/FullAgentTest.java
@@ -0,0 +1,17 @@
+package fr.pingtimeout.tyrion.agent;
+
+import org.junit.Test;
+
+public class FullAgentTest {
+    @Test(expected = NullPointerException.class)
+    public void should_() {
+        // Given
+
+
+        // When
+
+
+        // Then
+
+    }
+}
