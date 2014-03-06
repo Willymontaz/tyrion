61a56d6a13553bc432122be80a533f17c1f274f6
Pierre Laporte <pierre@pingtimeout.fr>
2014-03-06 22:39:02 +0100

    Cleaned samples/ directory

--- samples/compile.sh
+++ samples/compile.sh
@@ -2,6 +2,8 @@
 
 CURRENT_DIRECTORY=`dirname $0`
 
-javac -classpath classes -d classes ${CURRENT_DIRECTORY}/src/*.java
+rm -rf ${CURRENT_DIRECTORY}/classes
+mkdir -p ${CURRENT_DIRECTORY}/classes
+javac -classpath ${CURRENT_DIRECTORY}/classes -d ${CURRENT_DIRECTORY}/classes ${CURRENT_DIRECTORY}/src/*.java
 
 unset CURRENT_DIRECTORY
--- samples/run.sh
+++ samples/run.sh
@@ -2,7 +2,7 @@
 
 CURRENT_DIRECTORY=`dirname $0`
 
-grc -c conf.log java -javaagent:${CURRENT_DIRECTORY}/../agent/target/tyrion-agent-jar-with-dependencies.jar=output-file=${CURRENT_DIRECTORY}/simple-test-locks.log -cp ${CURRENT_DIRECTORY}/classes SimpleTest
+grc -c conf.log java "-javaagent:${CURRENT_DIRECTORY}/../agent/target/tyrion-agent-jar-with-dependencies.jar=output-file=${CURRENT_DIRECTORY}/logs/simple-test-locks.log;enabled-at-startup=true" -cp ${CURRENT_DIRECTORY}/classes SimpleTest
 
 unset CURRENT_DIRECTORY
 
